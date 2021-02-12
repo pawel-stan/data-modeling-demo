420f9917de8e9e7fb46c40adf3ed0240f0dbdf78
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2021-02-12 21:33:54 +0100

    Dodaje zależności do obsługi JPA

---
+++ src/main/java/edu/logintegra/datamappingdemo/DataMappingDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.logintegra.datamappingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMappingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataMappingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/logintegra/datamappingdemo/DataMappingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.logintegra.datamappingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMappingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
